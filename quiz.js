fa957e983ea75ec3a217f75c59966b9d8d82ec01
Inderway <47097657+Inderway@users.noreply.github.com>
2022-05-19 18:16:22 +0800

    add timer

--- quiz.js
+++ quiz.js
@@ -139,21 +139,113 @@ const shuffle = (f) => f.sort(() => Math.random() - 0.5);
 const questionNum = 5;
 const timeAllowed = 600;
 
-let timeLeftInSecond = timeAllowed;
-const timer = document.getElementById("timer");
+// let timeLeftInSecond = timeAllowed;
+// const timer = document.getElementById("timer");
 
-function countdown() {
-  timeLeftInSecond--;
-  let mins = Math.floor(timeLeftInSecond / 60);
-  let secs = Math.floor(timeLeftInSecond % 60);
+// function countdown() {
+//   timeLeftInSecond--;
+//   let mins = Math.floor(timeLeftInSecond / 60);
+//   let secs = Math.floor(timeLeftInSecond % 60);
 
-  timer.innerHTML = mins + ":" + secs;
+//   timer.innerHTML = mins + ":" + secs;
   
-  if (timeLeftInSecond <= 0) {
-    timer.innerHTML = "Time is up";
-    submit();
+//   if (timeLeftInSecond <= 0) {
+//     timer.innerHTML = "Time is up";
+//     submit();
+//   }
+// }
+
+//Timer
+const timer = document.getElementById("timer");
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 180;
+const ALERT_THRESHOLD = 60;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+let timePassed = 0;
+let timeLeft = timeAllowed;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+function onTimesUp() {
+  timer.innerHTML = "Time is up";
+  submit();
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = timeAllowed - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+  return timerInterval;
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
   }
 }
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / timeAllowed;
+  return rawTimeFraction - (1 / timeAllowed) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
 
 let counter = 0; // setInterval returns a number (ID)
 let correctCount = 0;
@@ -621,11 +713,36 @@ function setupPage() {
     e.style.height = "100%";
   }
 
-  //set up counter
-  timeLeftInSecond = timeAllowed;
-  counter = setInterval(countdown, 1000);
-  timer.style.display = "block";
 
+  //set up counter
+  //timeLeftInSecond = timeAllowed;
+  timer.style.display = "block";
+  
+  //counter = setInterval(countdown, 1000);
+  timer.innerHTML = `
+  <div class="base-timer">
+    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <g class="base-timer__circle">
+        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+        <path
+          id="base-timer-path-remaining"
+          stroke-dasharray="283"
+          class="base-timer__path-remaining ${remainingPathColor}"
+          d="
+            M 50, 50
+            m -45, 0
+            a 45,45 0 1,0 90,0
+            a 45,45 0 1,0 -90,0
+          "
+        ></path>
+      </g>
+    </svg>
+    <span id="base-timer-label" class="base-timer__label">${formatTime(
+      timeLeft
+    )}</span>
+  </div>
+  `;
+  counter =startTimer();
   submitButton.innerHTML = "提交";
   submitButton.removeEventListener("click", start);
   submitButton.addEventListener("click", submit);
