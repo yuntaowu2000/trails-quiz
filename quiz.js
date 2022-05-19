ccaff74a67819f735f7af1209600cd79d1462179
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2022-05-18 22:38:47 -0700

    integrated buttons (#3)

--- quiz.js
+++ quiz.js
@@ -1,5 +1,139 @@
+const container_anim = "<svg class=\"svg\" version=\"1.0\" width=\"40\" height=\"40\" preserveAspectRatio=\"xMidYMid meet\" viewBox=\"0 0 1280.000000 1278.000000\"><g transform=\"translate(0.000000,1278.000000) scale(0.100000,-0.100000)\" fill=\"#ffffff\" stroke=\"#355bae\"><path d=\"M7239 12760 c-20 -11 -46 -31 -57 -44 -11 -14 -154 -276 -318 -583 -206 -384 -311 -570 -339 -599 l-40 -42 -135 -7 c-74 -4 -193 -8 -265 -8 -199 -3 -155 -47 -582 570 -197 285 -376 537 -399 562 -37 40 -46 45 -95 48 -49 4 -104 -12 -633 -190 -397 -135 -586 -203 -603 -219 -48 -44 -55 -75 -43 -216 18 -225 69 -883 81 -1033 11 -140 10 -148 -11 -196 -12 -28 -33 -62 -48 -75 -15 -14 -116 -86 -225 -161 -207 -142 -241 -157 -321 -144 -18 3 -305 118 -639 256 -333 138 -619 251 -635 251 -70 0 -97 -28 -481 -505 -242 -300 -375 -473 -383 -497 -8 -29 -8 -48 0 -76 7 -23 155 -226 392 -537 209 -275 386 -514 395 -532 8 -17 15 -57 15 -88 0 -50 -14 -90 -110 -322 -61 -146 -121 -278 -133 -295 -49 -67 -57 -69 -747 -194 -700 -125 -699 -125 -733 -196 -24 -51 -91 -554 -122 -923 -8 -99 -18 -215 -21 -257 l-7 -78 39 -16 c21 -9 308 -115 637 -235 330 -120 613 -226 630 -236 18 -11 46 -39 62 -64 27 -40 36 -75 89 -364 62 -337 65 -383 30 -449 -12 -23 -206 -211 -499 -484 -426 -397 -482 -452 -494 -491 -12 -37 -12 -50 0 -85 24 -71 501 -989 529 -1018 15 -14 44 -31 66 -38 36 -11 89 0 665 129 345 78 641 141 659 141 80 0 118 -30 371 -294 132 -138 250 -267 260 -288 40 -78 41 -72 -131 -746 -119 -463 -161 -641 -156 -665 14 -75 20 -79 532 -367 516 -290 535 -299 604 -270 16 7 233 219 481 472 262 266 468 467 490 478 74 38 93 36 459 -45 372 -83 399 -92 444 -163 13 -20 114 -310 225 -643 146 -439 208 -612 224 -627 54 -50 93 -62 198 -62 55 0 142 4 192 10 51 5 178 17 282 25 291 24 546 55 576 70 33 17 77 69 84 98 3 12 64 301 136 642 83 398 137 630 149 650 43 69 69 83 379 200 260 99 311 115 359 115 32 -1 69 -6 84 -13 15 -7 242 -185 504 -397 262 -212 493 -395 513 -407 44 -28 104 -30 148 -6 57 33 925 697 941 721 8 13 17 44 20 70 4 43 -18 106 -228 657 -128 335 -234 623 -237 641 -11 69 21 130 178 344 83 113 163 218 177 234 38 40 98 66 157 66 28 0 325 -29 660 -65 588 -63 612 -65 652 -50 24 9 51 29 62 43 22 32 419 1114 427 1163 4 24 -2 48 -18 79 -20 40 -78 86 -508 402 -641 472 -587 429 -617 496 l-26 56 16 248 c16 263 26 307 78 354 11 10 284 151 607 314 l587 296 27 49 c30 54 33 76 18 103 -5 9 -66 277 -134 595 -82 380 -132 589 -145 610 -42 68 -18 65 -746 112 -613 40 -668 45 -709 65 -63 30 -80 54 -192 262 -93 173 -98 185 -98 243 0 36 7 72 17 92 9 17 159 271 333 565 174 293 321 548 326 565 22 79 21 81 -455 565 -243 247 -454 456 -469 464 -15 8 -48 14 -73 14 -41 0 -100 -30 -616 -316 -335 -186 -587 -320 -612 -326 -68 -14 -112 2 -294 105 -222 126 -249 156 -258 289 -3 40 -15 336 -26 658 -11 322 -24 599 -30 617 -11 36 -67 90 -102 98 -716 174 -1188 287 -1207 290 -14 2 -41 -5 -61 -15z m-549 -2620 c920 -78 1731 -450 2370 -1090 612 -611 985 -1393 1082 -2265 17 -154 17 -634 0 -775 -106 -876 -451 -1611 -1042 -2224 -594 -614 -1361 -995 -2245 -1113 -166 -22 -694 -25 -855 -5 -635 80 -1160 271 -1665 605 -602 399 -1088 979 -1371 1637 -229 533 -326 1067 -302 1661 36 903 412 1776 1048 2433 385 397 829 693 1345 894 301 117 653 202 965 231 63 6 131 13 150 14 81 8 414 6 520 -3z\"/></g></svg><svg height=\"50\" width=\"50\" class=\"svg_border\"><polygon points=\"23,5 5,23 28,40 40,28\" class = \"p_border\"/></svg>";
+const char_mapping={0:"A",1:"B",2:"C",3:"D",4:"E",5:"F"};
+
 const url="https://api.trails-game.com/"
-let data = [];
+let data = [{
+  "a": 0,
+  "explain": "要是他不是养子，全名就是「黎恩・奥斯本」吧",
+  "explain2": "再想想?来看看我们网站或者百度吧",
+  "options": [
+      {
+          "img": "",
+          "oid": 0,
+          "s": "舒华泽"
+      },
+      {
+          "img": "",
+          "oid": 1,
+          "s": "舒华兹"
+      },
+      {
+          "img": "",
+          "oid": 2,
+          "s": "舒派泽"
+      },
+      {
+          "img": "",
+          "oid": 3,
+          "s": "舒华塔"
+      }
+  ],
+  "question": {
+      "id": 1,
+      "img": "",
+      "s": "「闪之轨迹」主角黎恩姓什么？",
+      "t": "MCWithTextOnly"
+  }
+},
+{
+  "a": 0,
+  "explain": "肯恩和娜娜是对天真又有点成熟的可爱双胞胎呢",
+  "explain2": "再想想?来看看我们网站或者百度吧",
+  "options": [
+      {
+          "img": "",
+          "oid": 0,
+          "s": "娜娜"
+      },
+      {
+          "img": "",
+          "oid": 1,
+          "s": "莉娜"
+      },
+      {
+          "img": "",
+          "oid": 2,
+          "s": "蕾娜"
+      },
+      {
+          "img": "",
+          "oid": 3,
+          "s": "肯恩"
+      }
+  ],
+  "question": {
+      "id": 2,
+      "img": "",
+      "s": "以粉色头发给人留下深刻印象的女孩・悠娜，她的妹妹叫什么？",
+      "t": "MCWithTextOnly"
+  }
+},
+{
+  "a": 0,
+  "explain": "既是「地精」出身，又是施密特博士头号徒弟──发明了机甲兵的人吧",
+  "explain2": "再想想?来看看我们网站或者百度吧",
+  "options": [
+      {
+          "img": "",
+          "oid": 0,
+          "s": "弗兰兹"
+      },
+      {
+          "img": "",
+          "oid": 1,
+          "s": "特奥"
+      },
+      {
+          "img": "",
+          "oid": 2,
+          "s": "弗朗奇"
+      },
+      {
+          "img": "",
+          "oid": 3,
+          "s": "古恩"
+      }
+  ],
+  "question": {
+      "id": 3,
+      "img": "",
+      "s": "连保养导力装甲都难不倒的美女室长・亚莉莎，其父名为？",
+      "t": "MCWithTextOnly"
+  }
+},
+{
+  "a": 0,
+  "explain": "",
+  "explain2": "再想想? 或者百度",
+  "options": [
+      {
+          "img": "https://cdn.trails-game.com/wp-content/uploads/2021/07/未来_hajimari-150x150.png",
+          "oid": 0,
+          "s": "未来"
+      },
+      {
+          "img": "https://cdn.trails-game.com/wp-content/uploads/2021/07/圣典_hajimari-150x150.png",
+          "oid": 1,
+          "s": "圣典"
+      },
+      {
+          "img": "https://cdn.trails-game.com/wp-content/uploads/2021/07/处女_hajimari-150x150.png",
+          "oid": 2,
+          "s": "处女"
+      },
+      {
+          "img": "https://cdn.trails-game.com/wp-content/uploads/2021/07/水星_hajimari-150x150.png",
+          "oid": 3,
+          "s": "水星"
+      }
+  ],
+  "question": {
+      "id": 2001,
+      "img": "",
+      "s": "创轨中下列哪个核心回路拥有每回合自动回复HP的功能？",
+      "t": "MCWithImg"
+  }
+}];
 
 const shuffle = (f) => f.sort(() => Math.random() - 0.5);
 const questionNum = 5;
@@ -42,20 +176,17 @@ class Question {
     quizWrap.appendChild(qdiv);
 
     //create a container for question so that padding can be added.
-    let container = document.createElement("div");
-    container.className = "qnContainer";
-    qdiv.append(container);
-
-    let qnumber = document.createElement("div");
-    qnumber.className = "quizNumber";
-    qnumber.innerHTML = "第" + (this.i + 1) + "题";
-
+    let title = document.createElement("div");
+    title.className = "question-title";
+    title.id = "trailsQuizQn" + this.i;
+    title.innerHTML = "第" + (this.i + 1) + "题";
+    qdiv.append(title);
+    
     let question = document.createElement("div");
-    question.className = "trailsQuizQn";
+    question.className = "question-word";
     question.id = "trailsQuizQn" + this.i;
     question.innerHTML = this.currentQuestion.question.s;
-    question.appendChild(qnumber); 
-    container.appendChild(question);
+    qdiv.append(question);
 
     this.qdiv = qdiv;
     if (this.currentQuestion.question.img.startsWith("https://")) {
@@ -83,6 +214,11 @@ class Question {
 }
 
 class MCWithTextOnly extends Question {
+  constructor(data, i) {
+    super(data, i);
+    this.classes = "choosed", "choosed-word";
+  }
+
   show() {
     super.showQuestion();
 
@@ -90,69 +226,94 @@ class MCWithTextOnly extends Question {
     shuffle(this.currentQuestion.options);
     for (let a = 0; a < this.currentQuestion.options.length; a++) {
       
-      let label = document.createElement("div");
+      let container = document.createElement("div");
+      container.className = "container";
+      container.id = "q" + this.i + "a" + this.currentQuestion.options[a].oid + "container";
+      container.innerHTML = container_anim;
       
-      label.innerHTML = this.currentQuestion.options[a].s;
+      let letter = document.createElement("div");
+      letter.className = "letter";
+      letter.innerHTML = char_mapping[a];
+      container.appendChild(letter);
 
-      label.className = "trailsQuizAnsUnselected";
-      label.id = "q" + this.i + "a" + this.currentQuestion.options[a].oid;
+      let box = document.createElement("div");
+      box.id = "q" + this.i + "a" + this.currentQuestion.options[a].oid;
+      box.className = "box";
+      box.innerHTML = this.currentQuestion.options[a].s;
+      container.appendChild(box);
       
-      label.addEventListener("click", () => this.select(this.currentQuestion.options[a].oid));
-      this.answers.appendChild(label);
+      container.addEventListener("click", () => this.select(this.currentQuestion.options[a].oid));
+      this.answers.appendChild(container);
     }
   }
 
   select(a) {
     if (userAns[this.i] != -1) {
       let prevSelectedId = "q" + this.i + "a" + userAns[this.i];
-      document.getElementById(prevSelectedId).className = "trailsQuizAnsUnselected";
+      document.getElementById(prevSelectedId).classList.remove(this.classes);
     }
     console.log("selected " + a + "for q " + this.i);
     // select the new selection
     let selectedId = "q" + this.i + "a" + a;
     userAns[this.i] = a;
-    document.getElementById(selectedId).className = "trailsQuizAnsSelected";
+    document.getElementById(selectedId).classList.add(this.classes);
   }
 
   checkAns() {
     let selectedid = "q" + this.i + "a" + userAns[this.i];
+    console.log(selectedid);
     if (userAns[this.i] == this.getCorrectResult()) {
-        document.getElementById(selectedid).className = "trailsQuizAnsCorrect";
-        correctCount += 1;
-        userResult.push({"qid": this.currentQuestion.question.id, "result" : "c"});
+      document.getElementById(selectedid).className = "trailsQuizAnsCorrect";
+      correctCount += 1;
+      userResult.push({"qid": this.currentQuestion.question.id, "result" : "c"});
     } else {
-        if (userAns[this.i] != -1) {
-            document.getElementById(selectedid).className = "trailsQuizAnsWrong";
-        }
-        userResult.push({"qid": this.currentQuestion.question.id, "result" : "w"});
+      if (userAns[this.i] != -1) {
+          document.getElementById(selectedid).className = "trailsQuizAnsWrong";
+      }
+      userResult.push({"qid": this.currentQuestion.question.id, "result" : "w"});
     }
   }
 }
 
 class MCWithImg extends MCWithTextOnly {
+  constructor(data, i) {
+    super(data, i);
+    this.classes = "choosed", "choosed-img";
+  }
+
   show() {
     super.showQuestion();
 
     // shuffle the choices
     shuffle(this.currentQuestion.options);
     for (let a = 0; a < this.currentQuestion.options.length; a++) {
-      
-      let label = document.createElement("div");
 
-      // the current choice contains an image
+      let container = document.createElement("div");
+      container.className = "container";
+      container.id = "q" + this.i + "a" + this.currentQuestion.options[a].oid + "container";
+      container.innerHTML = container_anim;
+      
+      let letter = document.createElement("div");
+      letter.className = "letter";
+      letter.innerHTML = char_mapping[a];
+      container.appendChild(letter);
+
+      let box = document.createElement("div");
+      box.id = "q" + this.i + "a" + this.currentQuestion.options[a].oid;
+      box.className = "box";
+
       let img = document.createElement("img");
       img.src = this.currentQuestion.options[a].img;
-      label.appendChild(img);
+      box.appendChild(img);
 
-      let wordDes = document.createElement("div");
-      wordDes.innerHTML = this.currentQuestion.options[a].s;
-      label.appendChild(wordDes);
+      let wordDesc = document.createElement("div");
+      wordDesc.innerHTML = this.currentQuestion.options[a].s;
+      box.appendChild(wordDesc);
+
+      container.appendChild(box);
       
-      label.className = "trailsQuizAnsUnselected";
-      label.id = "q" + this.i + "a" + this.currentQuestion.options[a].oid;
-      
-      label.addEventListener("click", () => this.select(this.currentQuestion.options[a].oid));
-      this.answers.appendChild(label);
+      container.addEventListener("click", () => this.select(this.currentQuestion.options[a].oid));
+      this.answers.appendChild(container);
     }
   }
 }
@@ -163,11 +324,11 @@ class MCWithTextOnlyMultiAns extends MCWithTextOnly{
     if (userAns[this.i].indexOf(a) == -1) {
       // user has not selected this option before
       userAns[this.i].push(a);
-      document.getElementById(selectedId).className = "trailsQuizAnsSelected";
+      document.getElementById(selectedId).classList.add(this.classes);
     } else {
       // remove the selection
       userAns[this.i] = userAns[this.i].filter(data => data !== a);
-      document.getElementById(selectedId).className = "trailsQuizAnsUnselected";
+      document.getElementById(selectedId).classList.remove(this.classes);
     }
     console.log(userAns[this.i]);
   }
@@ -179,9 +340,9 @@ class MCWithTextOnlyMultiAns extends MCWithTextOnly{
       if (this.currentQuestion.a.indexOf(userAns[this.i][j]) == -1) {
         // user choice is not a correct answer
         correct = false;
-        document.getElementById(selectedId).className = "trailsQuizAnsWrong";
+        document.getElementById(selectedId).classList.add("trailsQuizAnsWrong");
       } else {
-        document.getElementById(selectedId).className = "trailsQuizAnsCorrect";
+        document.getElementById(selectedId).classList.add("trailsQuizAnsCorrect");
       }
     }
 
@@ -208,11 +369,11 @@ class MCWithImgMultiAns extends MCWithImg{
     if (userAns[this.i].indexOf(a) == -1) {
       // user has not selected this option before
       userAns[this.i].push(a);
-      document.getElementById(selectedId).className = "trailsQuizAnsSelected";
+      document.getElementById(selectedId).classList.add(this.classes);
     } else {
       // remove the selection
       userAns[this.i] = userAns[this.i].filter(data => data !== a);
-      document.getElementById(selectedId).className = "trailsQuizAnsUnselected";
+      document.getElementById(selectedId).classList.remove(this.classes);
     }
     console.log(userAns[this.i]);
   }
@@ -224,9 +385,9 @@ class MCWithImgMultiAns extends MCWithImg{
       if (this.currentQuestion.a.indexOf(userAns[this.i][j]) == -1) {
         // user choice is not a correct answer
         correct = false;
-        document.getElementById(selectedId).className = "trailsQuizAnsWrong";
+        document.getElementById(selectedId).classList.add("trailsQuizAnsWrong");
       } else {
-        document.getElementById(selectedId).className = "trailsQuizAnsCorrect";
+        document.getElementById(selectedId).classList.add("trailsQuizAnsCorrect");
       }
     }
 
@@ -315,10 +476,9 @@ function removeInputListeners() {
   for (let i = 0; i < data.length; i++) {
     if (data[i].question.t != "Text") {
       for (let a = 0; a < data[i].options.length; a++) {
-        let id = "q" + i + "a" + data[i].options[a].oid;
-        let currElem = document.getElementById(id);
-        currElem.className = "unselectedAnswer";
-        currElem.replaceWith(currElem.cloneNode(true));
+        let id = "q" + i + "a" + data[i].options[a].oid + "container";
+        let currContainer = document.getElementById(id);
+        currContainer.replaceWith(currContainer.cloneNode(true));
       }
     } else {
       let id = "q" + i + "input";
@@ -336,8 +496,8 @@ async function showExplanationAndResult() {
       "Content-Type": "application/json",
     }
   }
-  let qstatsdata = await fetch(url + "quiz-stats", options);
-  qStats = await qstatsdata.json();
+  // let qstatsdata = await fetch(url + "quiz-stats", options);
+  // qStats = await qstatsdata.json();
   console.log(qStats);
 
   let qs = document.getElementsByClassName("question");
@@ -462,9 +622,9 @@ function setupPage() {
 
 
 async function start() {
-  let result = await fetch(url + "quiz-questions?qn=" + questionNum);
-  console.log(result);
-  data = await result.json();
+  // let result = await fetch(url + "quiz-questions?qn=" + questionNum);
+  // console.log(result);
+  // data = await result.json();
   setupPage();
   window.scrollTo(screenLeft, screenTop);
 }
